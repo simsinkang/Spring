322565d09f6a462466292b8b016f1c26c00bb34b
0505ssk <0505ssk@naver.com>
2019-06-12 14:05:10 +0900

    14:03

--- resources/static/resources/js/index.js
+++ resources/static/resources/js/index.js
@@ -1,22 +1,39 @@
-alert("자바스크립트 연결 성공 !!");
+var app = (function(){
+    let init = function(){ 
+        login_form();
+    }
+    let login_form = function(){
+    var wrapper = document.querySelector('#wrapper');
+    
 
-var wrapper = document.querySelector('#wrapper');
-wrapper.innerHTML = '<h1>SPA 시작</h1>'
-+'<div id="target">삭제할 내용</div>'
-+'<button id="btn">지워버리자</button>'
-+'<button id="btn2">추가하자</button>';
+    wrapper.innerHTML = '<form action="/action_page.php">'
+    +'  First name:<br>'
+    +'  <input type="text" name="firstname" value="Mickey">'
+    +'  <br>'
+    +'  Last name:<br>'
+    +'  <input type="text" name="lastname" value="Mouse">'
+    +'  <br><br>'
+    +'  <input type="submit" value="Submit">'
+    +'  <input id = "target" type="submit" value="Join">'
+    +'</form> ';
 
-var btn = document.querySelector('#btn');
-var target = document.getElementById('target');
-
-btn.addEventListener('click', function(){
-    target.innerHTML = '';
-
-});
-
-var btn2 = document.querySelector('#btn2');
-
-btn2.addEventListener('click', function() {
-    var temp = document.createTextNode('새로 추가됨');
-    target.appendChild(temp);
-});
+    
+    
+    
+    var btn = document.querySelector('#btn');
+    var target = document.getElementById('target');
+    
+    btn.addEventListener('click', function(){
+        target.innerHTML = '';
+    
+    });
+    
+    var btn2 = document.querySelector('#btn2');
+    
+    btn2.addEventListener('click', function() {
+        var temp = document.createTextNode('새로 추가됨');
+        target.appendChild(temp);
+    });
+  }
+  return {init : init};
+})();
